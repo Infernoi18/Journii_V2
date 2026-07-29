84ef98c824b915d3a8c0ac91b35b85f2c232eb07
Infernoi18 <kumar.ayush.offixial@gmail.com>
2026-07-29 11:54:26 +0530

    Added AppContainer.kt

--- app/src/main/java/com/example/journii_version2/core/di/AppContainer.kt
+++ app/src/main/java/com/example/journii_version2/core/di/AppContainer.kt
@@ -5,10 +5,13 @@ import com.example.journii_version2.core.data.inspiration.FakeInspirationReposit
 import com.example.journii_version2.core.data.inspiration.InspirationRepository
 import com.example.journii_version2.core.data.profile.FakeProfileRepository
 import com.example.journii_version2.core.data.profile.ProfileRepository
+import com.example.journii_version2.core.data.wishlist.OfflineWishlistRepository
+import com.example.journii_version2.core.data.wishlist.WishlistRepository
 import com.example.journii_version2.core.security.session.SecureTokenStore
 
 class AppContainer(context: Context) {
     val secureTokenStore: SecureTokenStore = SecureTokenStore(context.applicationContext)
     val inspirationRepository: InspirationRepository = FakeInspirationRepository()
     val profileRepository: ProfileRepository = FakeProfileRepository(inspirationRepository)
+    val wishlistRepository: WishlistRepository = OfflineWishlistRepository()
 }
